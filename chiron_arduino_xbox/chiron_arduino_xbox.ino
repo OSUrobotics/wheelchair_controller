b2aa4a001e05fa6e4d36ccacea9afbe680994139
benjaminnarin <narinb@onid.oregonstate.edu>
2014-06-27 10:59:02 -0700

    First Commit

---
+++ chiron_arduino_xbox/chiron_arduino_xbox.ino
@@ -0,0 +1,153 @@
+/*
+This is the aduino code for AJ 2.0, for electrical designs and documentation
+check the GIT Hub located at ...
+ */
+
+//include rosserial libaries
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+//#include <std_msgs/ByteMultiArray.h>
+
+ros::NodeHandle nh;
+
+// set pin numbers:
+const int forward_pin = 2;     // the number of the pushbutton pin
+const int reverse_pin = 3;
+const int left_pin = 4;	//setting pins for motor controller
+const int right_pin = 5;
+const int fifth_switch_pin = 6;
+
+boolean forward_bool = false;
+boolean reverse_bool = false;
+boolean left_bool = false;
+boolean right_bool = false;
+boolean fifth_switch_bool = false;
+
+void bool_callback( const std_msgs::UInt16& bool_value){
+	if(bool_value.data == 1){
+		forward_bool = true;	    
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 2){
+		forward_bool = false;    
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 3){
+		reverse_bool = true;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 4){
+		reverse_bool = false;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 5){
+		left_bool = true;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 6){
+		left_bool = false;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 7){
+		right_bool = true;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 8){
+		right_bool = false;
+		//bool_array_update();
+	}
+
+	if(bool_value.data == 9){
+		fifth_switch_bool = true;
+		//bool_array_update();
+	
+	}
+
+	if(bool_value.data == 10){
+		fifth_switch_bool = false;
+		//bool_array_update();
+	}
+	else{
+		return; 
+	}	    
+}
+
+ros::Subscriber<std_msgs::UInt16> sub_1("/bool", bool_callback);
+
+void setup(){
+	// initialize the pushbutton pin as an input:
+	pinMode(forward_pin, OUTPUT);     
+	pinMode(reverse_pin, OUTPUT);
+	pinMode(left_pin, OUTPUT);
+	pinMode(right_pin, OUTPUT);
+	pinMode(fifth_switch_pin, OUTPUT);
+
+	nh.initNode();
+	
+	forward_bool = false;
+	reverse_bool = false;
+	left_bool = false;
+	right_bool = false;
+	fifth_switch_bool = false;
+	
+	nh.subscribe(sub_1);
+	}
+
+void loop(){
+	if(forward_bool == true){
+		digitalWrite(forward_pin, HIGH);	
+	}
+	if(forward_bool == false){
+		digitalWrite(forward_pin, LOW);
+	}	
+
+	if(reverse_bool == true){
+		digitalWrite(reverse_pin, HIGH);	
+	}
+	if(reverse_bool == false){
+		digitalWrite(reverse_pin, LOW);
+	}	
+
+	if(left_bool == true){
+		digitalWrite(left_pin, HIGH);
+	}
+	if(left_bool == false){
+		digitalWrite(left_pin, LOW);
+	}
+
+	if(right_bool == true){
+		digitalWrite(right_pin, HIGH);
+	}
+	if(right_bool == false){
+		digitalWrite(right_pin, LOW);
+	}
+
+	if(fifth_switch_bool == true){
+		digitalWrite(fifth_switch_pin, HIGH);
+	}
+	if(fifth_switch_bool == false){
+		digitalWrite(fifth_switch_pin, LOW);
+	}
+
+	nh.spinOnce();
+}
---
+++ scripts/xbox_chiron_control.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+#Current Up To Date Usable Node
+# Every python controller needs these lines
+import roslib 
+roslib.load_manifest('chiron_controller')
+import rospy
+from std_msgs.msg import UInt16
+from sensor_msgs.msg import Joy
+# The velocity command message
+
+class controller:
+	def __init__(self):
+		
+		self.forward_true = 1
+		self.forward_false = 2
+		self.reverse_true = 3
+		self.reverse_false = 4
+		self.left_true = 5
+		self.left_false = 6
+		self.right_true = 7
+		self.right_false = 8
+		self.fifth_switch_true = 9
+		self.fifth_switch_false = 10
+		
+		#Subscribes to /basescan
+		print "start Init"
+		self.joy_sub = rospy.Subscriber("/joy",Joy,self.joy_callback)
+		self.bool_pub = rospy.Publisher('/bool', UInt16)
+		print "End Init"
+
+	def joy_callback(self, joy_values): 
+		self.axes = joy_values.axes
+		self.buttons = joy_values.buttons
+
+		if (self.buttons[13] == 1):
+			self.bool_pub.publish(self.forward_true)
+		if (self.buttons[13] == 0):
+			self.bool_pub.publish(self.forward_false)
+
+		if (self.buttons[14] == 1):
+			self.bool_pub.publish(self.reverse_true)
+		if (self.buttons[14] == 0):
+			self.bool_pub.publish(self.reverse_false)
+	
+		if (self.buttons[11] == 1):
+			self.bool_pub.publish(self.left_true)
+		if (self.buttons[11] == 0):
+			self.bool_pub.publish(self.left_false)
+		
+		if (self.buttons[12] == 1):
+			self.bool_pub.publish(self.right_true)
+		if (self.buttons[12] == 0):
+			self.bool_pub.publish(self.right_false)
+
+		if (self.buttons[4] == 1):
+			self.bool_pub.publish(self.fifth_switch_true)
+		if (self.buttons[4] == 0):
+			self.bool_pub.publish(self.fifth_switch_false)
+
+
+
+if __name__ == '__main__':
+	rospy.init_node('chiron_controller',log_level=rospy.DEBUG)
+	controller = controller()
+	rospy.spin()
